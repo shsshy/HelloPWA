ca1d06f2bbbf032fd62a2661beb0d48ae52fb4ce
shsshy <60872258+shsshy@users.noreply.github.com>
2020-02-10 16:18:08 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+var cacheName = 'Hello-PWASC';
+
+var filesToCache = [
+'/',
+'/index.html',
+'/css/style.css',
+'/js/main.js',
+];
+
+
+/* Start the servie worker and cache all of the app's cotent */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cachName).then(function(cache) {
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e){
+
+    e.respondWith(
+        caches.match(e.request).then
+            (function(response){
+                return response || fetch(e.request);
+            }
+        )   
+    );
+});
